43fc7d8d90fa921a2e42d82faefa113a0c37aa16
HughStanway <hugh.stanway1@gmail.com>
2025-11-16 15:53:39 +0000

    version 0.10 - serve some routes

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@
 
 #include "logger.h"
 #include "server.h"
+#include "router.h"
+#include "http_response_builder.h"
 
 HTTPServer::Server *g_server = nullptr;
 
@@ -17,6 +19,18 @@ void signal_handler(int signum) {
 int main() {
     HTTPServer::Server server(HTTPServer::Port(8080));
     g_server = &server;
+
+    HTTPServer::Router::instance().addRoute("GET", "/", [](const HTTPServer::HttpRequest& req) {
+    return HTTPServer::Responses::ok(req, "Welcome to the home page!");
+    });
+
+    HTTPServer::Router::instance().addRoute("GET", "/about", [](const HTTPServer::HttpRequest& req) {
+        return HTTPServer::Responses::ok(req, "About page");
+    });
+
+    HTTPServer::Router::instance().addRoute("POST", "/submit", [](const HTTPServer::HttpRequest& req) {
+        return HTTPServer::Responses::ok(req, "Form submitted!");
+    });
 
     std::signal(SIGINT, signal_handler);
 
